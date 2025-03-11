ae24e84bf4214952756509e95a164eb3fd6cf9cf
Abhi1234cs21 <kamleshadajariya6252@gmail.com>
2025-03-11 13:36:37 +0530

    Lombok Dependency added

--- src/main/java/com/example/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeeDTO.java
@@ -10,27 +10,5 @@ public class EmployeeDTO {
     private String department;
     private double salary;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
